123e32e39b5bba173e76a97baf5d8fb8bc309c32
Lucas Carvalho <barraroot@gmail.com>
2023-01-11 23:39:48 -0300

    Instalação base do inertia

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+
+<template>
+    <Head title="Bem vindo" />
+    <div>
+        <h1>Bem Vindo Inertia App.</h1>
+        <p>Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})</p>
+    </div>
+</template>
